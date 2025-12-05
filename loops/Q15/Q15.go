e85c780a82edf7a1100b07dfae7b4abdd38209c2
theworld <136832190+sandiptamangdev@users.noreply.github.com>
2025-12-05 22:01:50 +0545

    feat: completed question no 15
    Go has slices → dynamic, resizable.
    Go has arrays → fixed size, safer + predictable.
    Python lists are always dynamic.
    Go’s range lets you explicitly choose whether to use the index.

--- loops/Q15/Q15.go
+++ loops/Q15/Q15.go
@@ -1,2 +1,22 @@
 // Loop through a slice and print each item.
 
+// package main
+// import "fmt"
+
+// func main() {
+// 	s := []int{1, 2, 3}
+
+// 	for i 
+// }
+
+package main
+
+import "fmt"
+
+func main() {
+	s := []int{1, 2, 3}
+
+	for _, v := range s {
+		fmt.Println(v)
+	}
+}
